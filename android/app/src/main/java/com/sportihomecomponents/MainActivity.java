43169e45e31d6a0ebe3ea862aff4a3e0507df4c3
Raphael <contact@raphaelgimenez.fr>
2020-11-05 10:43:03 +0100

    chore: change project name to "YourDesignSystem"

--- android/app/src/main/java/com/sportihomecomponents/MainActivity.java
+++ android/app/src/main/java/com/sportihomecomponents/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sportihomecomponents;
+package com.yourdesignsystem;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SportihomeComponents";
+    return "YourDesignSystem";
   }
 }
--- ios/YourDesignSystemTests/YourDesignSystemTests.m
+++ ios/YourDesignSystemTests/YourDesignSystemTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SportihomeComponentsTests : XCTestCase
+@interface YourDesignSystemTests : XCTestCase
 
 @end
 
-@implementation SportihomeComponentsTests
+@implementation YourDesignSystemTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
